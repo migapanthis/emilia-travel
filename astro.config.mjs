b2cfad852a0531bdb8ccf3d0a7ab2858dde2dc5b
migapanthis <mig@cartoonbeats.com>
2026-08-12 20:13:40 +0300

    edit site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://migapanthis.github.io.com',
+  site: 'https://migapanthis.github.io',
   base: '/emilia-travel'
 });
