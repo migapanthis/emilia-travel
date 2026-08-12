fe58c878b1714637c0b8b66da88441a75de3e0e9
migapanthis <mig@cartoonbeats.com>
2026-08-12 21:42:44 +0300

    refactor: remove PostList component and update homepage to render blog entries directly

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Emilia Travel';
+export const SITE_DESCRIPTION = 'Pictures and descriptions taken during my travels';
 
 const rawBase = import.meta.env.BASE_URL;
 export const BASE_PATH = rawBase.endsWith('/') ? rawBase : `${rawBase}/`;
